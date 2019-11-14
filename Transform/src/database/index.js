51010d766cdf3f668bf4c29a0446bafcd42db18e
Marlon Leoner <marlon.leoner@gmail.com>
2019-11-14 19:22:56 -0300

    Transform folder created (Q4)

---
+++ Transform/src/database/index.js
@@ -0,0 +1,137 @@
+const path = require("path")
+const fs = require("fs")
+
+module.exports = function () {
+   const filename = "GioMovies.json"
+   const filepath = path.resolve(__dirname, "..", "..", "..", filename)
+   const JSONFile = fs.readFileSync(filepath)
+
+   // Create JSON Object
+   const JSONObject = JSON.parse(JSONFile.toString())
+
+   const AllTopics = JSONObject.topicMap.topic
+
+   var AllTopicMovies = []
+   var AllYears = []
+   var AllGenres = []
+   var AllDurations = []
+   var AllDirectors = []
+   var AllActors = []
+   AllTopics.forEach(topic => {
+      if (topic.instanceOf) {
+         const obj = {
+            id: topic._id,
+            value: topic.baseName.baseNameString
+         }
+
+         switch (topic.instanceOf.topicRef._href) {
+            case "#Filme":
+               AllTopicMovies.push(topic)
+               break
+            case "#Ano":
+               AllYears.push(obj)
+               break
+            case "#Genero":
+               AllGenres.push(obj)
+               break
+            case "#Duracao":
+               AllDurations.push(obj)
+               break
+            case "#Direcao":
+               AllDirectors.push(obj)
+               break
+            case "#Elenco":
+               AllActors.push(obj)
+               break
+         }
+      }
+   })
+
+   var AllMovies = []
+   AllTopicMovies.forEach(movie => {
+      const lenOccurrences = movie.occurrence.length
+      if (!lenOccurrences) {
+         movie.occurrence = Array(movie.occurrence)
+      }
+
+      var occ = {
+         id: movie._id,
+         titulo: movie.baseName.baseNameString,
+         en_titulo: "",
+         distribuicao: "",
+         sinopse: "",
+         elencoApoio: [],
+         site: "",
+         ano: "",
+         diretor: {},
+         duracao: "",
+         atores: [],
+         genero: {}
+      }
+
+      movie.occurrence.forEach(occurrence => {
+         if (occurrence.scope) {
+            switch (occurrence.scope.topicRef._href) {
+               case '#ingles':
+                  occ.en_titulo = occurrence.resourceData
+                  break
+               case '#distribuicao':
+                  occ.distribuicao = occurrence.resourceData
+                  break
+               case '#sinopse':
+                  occ.sinopse = occurrence.resourceData
+                  break
+               case '#elencoApoio':
+                  occ.elencoApoio = [...occ.elencoApoio, occurrence.resourceData]
+                  break
+            }
+         }
+         else if (occurrence.instanceOf) {
+            switch (occurrence.instanceOf.topicRef._href) {
+               case '#site':
+                  occ.site = occurrence.resourceRef._href
+                  break
+            }
+         }
+      })
+
+      AllMovies.push(occ)
+   })
+
+   const Associations = JSONObject.topicMap.association
+   Associations.forEach(association => {
+      const movie_id = association.member[0].topicRef._href
+      const assoc_id = association.member[1].topicRef._href
+
+      const movie_index = AllMovies.findIndex(m => ("#" + m.id) === movie_id)
+
+      switch (association.instanceOf.topicRef._href) {
+         case "#filme-ano":
+            const year = AllYears.find(y => ("#" + y.id) === assoc_id)
+            AllMovies[movie_index].ano = year.value
+            break
+         case "#filme-direcao":
+            const director = AllDirectors.find(d => ("#" + d.id) === assoc_id)
+            AllMovies[movie_index].diretor = { id: director.id, value: director.value }
+            break
+         case "#filme-duracao":
+            const duration = AllDurations.find(d => ("#" + d.id) === assoc_id)
+            AllMovies[movie_index].duracao = duration.value
+            break
+         case "#filme-elenco":
+            var actor = AllActors.find(a => ("#" + a.id) === assoc_id)
+            if (!actor) {
+               actor = AllDirectors.find(d => ("#" + d.id) === assoc_id)
+            }
+            AllMovies[movie_index].atores = [...AllMovies[movie_index].atores, { id: actor.id, value: actor.value }]
+            break
+         case "#filme-genero":
+            const genre = AllGenres.find(g => ("#" + g.id) === assoc_id)
+            AllMovies[movie_index].genero = { id: genre.id, value: genre.value }
+            break
+      }
+   })
+
+   return AllMovies
+}
+
---
+++ Transform/src/index.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const path = require("path")
+
+const app = express()
+
+const GetAllMovies = require("./database")
+
+nunjucks.configure(path.resolve(__dirname, 'public'), {
+   autoescape: true,
+   express: app,
+   watch: true
+})
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
+
+const AllMovies = GetAllMovies()
+
+app.get('/', (req, res) => {
+   return res.render('views/index', { movies: AllMovies })
+})
+
+const idMiddleware = (req, res, next) => {
+   const { id } = req.params
+   if (!id) {
+      return res.redirect('/')
+   }
+
+   return next()
+}
+
+app.get('/movie/:id', idMiddleware, (req, res) => {
+   const { id } = req.params
+
+   const movie = AllMovies.find(m => m.id === id)
+   if (!movie) {
+      return res.redirect('/')
+   }
+
+   return res.render("views/movies", { movie })
+})
+
+app.get('/actor/:id', (req, res) => {
+   const { id } = req.params
+
+   const movies = AllMovies.filter(m => m.atores.find(a => a.id === id))
+   const actor = movies[0].atores.find(a => a.id === id)
+   if (!movies) {
+      return res.redirect('/')
+   }
+
+   return res.render("views/actors", { movies, actor })
+})
+
+app.get('/director/:id', (req, res) => {
+   const { id } = req.params
+
+   const movies = AllMovies.filter(m => m.diretor.id === id)
+   const director = movies[0].diretor
+   if (!movies) {
+      return res.redirect('/')
+   }
+
+   return res.render("views/directors", { movies, director })
+})
+
+app.get('/year/:id', (req, res) => {
+   const { id } = req.params
+
+   const movies = AllMovies.filter(m => m.ano === id)
+   if (!movies) {
+      return res.redirect('/')
+   }
+
+   return res.render("views/years", { movies, year: id })
+})
+
+app.get('/duration/:id', (req, res) => {
+   const { id } = req.params
+
+   const movies = AllMovies.filter(m => m.duracao === id)
+   if (!movies) {
+      return res.redirect('/')
+   }
+
+   return res.render("views/durations", { movies, duration: id })
+})
+
+app.get('/genre/:id', (req, res) => {
+   const { id } = req.params
+
+   const movies = AllMovies.filter(m => m.genero.id === id)
+   const genre = movies[0].genero
+   if (!movies) {
+      return res.redirect('/')
+   }
+
+   return res.render("views/genres", { movies, genre })
+})
+
+app.use(function (req, res, next) {
+   res.redirect('/')
+});
+
+app.listen(3000)
